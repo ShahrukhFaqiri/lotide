278facd4eb908c6170e6eedc8caf9a1d34353797
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-08 01:10:29 +0000

    Added console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = (actual, expected) => {
+
+  if (actual !== expected) {
+    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+  } else {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
