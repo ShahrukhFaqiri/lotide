541f78d19c02f340f86f0f182db9546e491cd115
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-08 01:14:27 +0000

    Better readibility

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = (actual, expected) => {
 
   if (actual !== expected) {
-    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑 Assertion Failed:  ${actual} !==  ${expected}`);
   } else {
-    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   }
 
 };
