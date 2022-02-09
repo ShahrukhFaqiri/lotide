3713b2de5ddc8ad0e470300ef709c9df447655f7
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-09 04:56:19 +0000

    Ability to filter the list of a given function

---
+++ without.js
@@ -0,0 +1,21 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2) !== true) {
+    console.log(`🛑 Assertion Failed:  ${arr1} !==  ${arr2}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+}
+
+v
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without functionv
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
