3994ae2279bd1117f80cf0d0a0909d38134e22d7
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-10 04:09:36 +0000

    implementation of eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,36 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion Failed:  ${actual} !==  ${expected}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const eqObject = (object1, object2) => {
+  let keyArrOb1 = Object.keys(object1);
+  let keyArrOb2 = Object.keys(object2);
+  if (keyArrOb1.length !== keyArrOb2.length) return false;
+  for (const keys in object1) {
+    if (Array.isArray(object1[keys]) && Array.isArray(object2[keys])) {
+     return eqArrays(object1[keys], object2[keys]);
+    }
+    if (object1[keys] !== object2[keys]) return false;
+  }
+  return true;
+};
+
+// const abc = { a: "1", b: "2", c: "3" };
+const cd = { a: "2", b: "2" };
+// const ba = { b: "2", a: "2" };
+// const cd = { c: "1", d: ["2", 4] };
+const dc = { d: ["2", 4], c: "1" };
+const cd2 = { c: "1", d: ["2"]};
+assertEqual(eqObject(cd, cd2), false);
