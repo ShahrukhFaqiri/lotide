512ec91f8bee9d7b8a41b9058885a424ce604014
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-10 05:27:59 +0000

    assertObjects equal implementation

---
+++ assertObjectsEqual.js
@@ -0,0 +1,35 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const eqObject = (object1, object2) => {
+  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+  for (const keys in object1) {
+    if (Array.isArray(object1[keys]) && Array.isArray(object2[keys])) {
+      if (!eqArrays(object1[keys], object2[keys])) {
+        return false;
+      }
+    } else if (object1[keys] !== object2[keys]) return false;
+  }
+  return true;
+};
+
+const assertObjectsEqual = (actual, expected) => {
+  const inspect = require('util').inspect
+  if (eqObject(actual, expected) !== true) {
+    console.log(`🛑 Assertion Failed:  ${inspect(actual)} !==  ${inspect(expected)}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  }
+};
+
+// const ba = { b: "2", a: "2" };
+const ba = { d: ["2", 4], c: "2" }
+const cd =  { d: ["2", 4], c: "2" }
+// const dc = { d: ["2", 4], c: "1" };
+// const cd2 = { c: "1", d: ["2"]};
+assertObjectsEqual({ c: "2" , d: ["2", 4]}, { d: ["2", 4], c: "2" });
