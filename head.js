09440e4dffed2c21e527ceb652412d9f07a04c2a
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-14 22:04:06 +0000

    removing test case from function module, added test folder for function test

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = (actual, expected) => {
-
-  if (actual !== expected) {
-    console.log(`🛑 Assertion Failed:  ${actual} !==  ${expected}`);
-  } else {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
-
 const head = (array) => {
   return array[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
