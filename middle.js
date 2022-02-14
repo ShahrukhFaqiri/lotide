db9fe916c30cfe5982215cbc4769efa7230ba266
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-14 23:54:28 +0000

    middle refactored, added test file for middle function

--- middle.js
+++ middle.js
@@ -1,19 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2) !== true) {
-    console.log(`🛑 Assertion Failed: ${arr1} !==  ${arr2}`);
-  } else {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  }
-};
-
 const middle = (array) => {
   let halved = array.length / 2;
   let result = [];
@@ -30,5 +14,4 @@ const middle = (array) => {
   return result;
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
+module.exports = middle;
