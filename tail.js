1672a99e965a722c0abd6e226ebc0616d025159e
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-14 22:24:05 +0000

    removed testing from tail function, added testing in tailTest module

--- tail.js
+++ tail.js
@@ -1,18 +1,6 @@
-const assertEqual = (actual, expected) => {
-  let message = '';
-  if (actual !== expected) {
-    message = `🛑 Assertion Failed:  ${actual} !==  ${expected}`;
-  } else {
-    message = `✅ Assertion Passed: ${actual} === ${expected}`;
-  }
-  return message; //Function was giving undefined.
-};
-
 const tail = (array) => {
   let newArray = array.slice(array[0]);
   return newArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-console.log(assertEqual(words.length, 3));
+module.exports = tail;
