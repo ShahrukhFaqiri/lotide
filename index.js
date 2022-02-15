91ab1130f9e0406effe434409e772380de5725de
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-15 04:49:20 +0000

    Added the revamped count only to the index file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const head = require('./head');
 const tail = require('./head');
 const middle = require ('./middle');
+const countOnly = require('./countOnly')
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  countOnly
 }
