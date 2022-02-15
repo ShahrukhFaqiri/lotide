66da09f89b4a92980caedf12afdbe0ca218ef836
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-15 05:33:58 +0000

    eqArray test file addition

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,21 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
+describe('#eqArrays', () => {
+  it('should return true if the arrays are identical', () => {
+    const actual = eqArrays([1, 2, 3], [1, 2, 3]);
+    const expected = true;
+    assert.strictEqual(actual, expected);
+  });
+  it('should return false if the arrays are NOT identical', () => {
+    const actual = eqArrays([1, 2, 3, 4], [1, 2, 3]);
+    const expected = false;
+    assert.strictEqual(actual, expected);
+  });
+  it('should return false if the arrays are same size but not identical', () => {
+    const actual = eqArrays([1, 2, 3], [1, 2, 4]);
+    const expected = false;
+    assert.strictEqual(actual, expected);
+  });
+});
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
