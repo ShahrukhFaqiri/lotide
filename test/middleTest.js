05cd56b77ea21c2272df0a7955b83fdb167c7900
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-15 00:57:33 +0000

    Revamped the middle test file to utilize mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
+describe('#MIDDLE', () => {
+  it(`should return the two middle indexes of the array`, () => {
+    const actual = middle([1, 2, 3, 4]);
+    const expected = [2, 3];
+    assert.deepEqual(actual, expected);
+  });
+  it(`should return empty array if array has 2 or less elements`, () => {
+    const actual = middle([1]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+});
